73653e71b436e01d6656744227b04ad4f019faf1
alim <sourav.0.alim@gmail.com>
2025-07-11 02:47:23 +0600

    Adding android as webcam

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,9 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
+    alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
@@ -14,9 +17,28 @@ kotlin {
             kotlinOptions.jvmTarget = "11"
         }
     }
+
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     
     sourceSets {
         val desktopMain by getting
+
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation("androidx.activity:activity-compose:1.10.1")
+
+            implementation("androidx.camera:camera-camera2:1.3.0")
+            implementation("androidx.camera:camera-lifecycle:1.3.0")
+            implementation("androidx.camera:camera-view:1.3.0")
+            implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+            implementation("org.java-websocket:Java-WebSocket:1.5.2")
+        }
         
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -73,6 +95,34 @@ kotlin {
         }
     }
 }
+
+android {
+    namespace = "com.alim"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+//
+    defaultConfig {
+        applicationId = "com.alim"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
 
 
 compose.desktop {
