b0d1768881548b537955d68d026ab661c82ef864
alim <sourav.0.alim@gmail.com>
2025-07-10 21:30:21 +0600

    little changes

--- server/src/main/kotlin/com/gub/routes/DashboardRoutes.kt
+++ server/src/main/kotlin/com/gub/routes/DashboardRoutes.kt
@@ -7,6 +7,7 @@ import com.gub.services.WeatherService
 import io.ktor.server.application.Application
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
 fun Application.dashboardRoutes() {
